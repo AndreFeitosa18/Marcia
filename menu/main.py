e85b88526b445570a889017379646f9aa9fe299c
AndreFeitosa18 <andrefeitosa1800@gmail.com>
2024-10-24 03:23:33 -0300

    Added missing docstrings in class "menu.main.MainMenu".

--- menu/main.py
+++ menu/main.py
@@ -3,7 +3,9 @@ from telebot.types import Message
 
 
 class MainMenu:
+	""" The BOT main menu class """
 	def register_response_methods(self, bot: TeleBot) -> None:
+		""" The method that records response methods associated with commands """
 		# Decorates the response function to the '/help' command:
 		@bot.message_handler(commands=['help'])
 		def help_command(message: Message) -> None:
